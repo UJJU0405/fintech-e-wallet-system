a93b72fd947e4d5a7370437b94ec83d29950b83d
UJJU0405 <pandeyujju536@gmail.com>
2026-04-16 18:08:34 +0530

    Create JwtUtil.java

---
+++ JwtUtil.java
@@ -0,0 +1,28 @@
+package com.fintech.ewallet.security;
+
+import io.jsonwebtoken.*;
+import org.springframework.stereotype.Component;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+
+    private final String SECRET = "secret123";
+
+    public String generateToken(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24))
+                .signWith(SignatureAlgorithm.HS256, SECRET)
+                .compact();
+    }
+
+    public String validateToken(String token) {
+        return Jwts.parser()
+                .setSigningKey(SECRET)
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+}
