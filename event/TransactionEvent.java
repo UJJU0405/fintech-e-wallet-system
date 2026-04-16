cf8548b8645f9062893163772548e82e006fa93f
UJJU0405 <pandeyujju536@gmail.com>
2026-04-16 18:04:25 +0530

    Create TransactionEvent.java

---
+++ event/TransactionEvent.java
@@ -0,0 +1,18 @@
+package com.fintech.ewallet.event;
+
+public class TransactionEvent {
+
+    private String sender;
+    private String receiver;
+    private Double amount;
+
+    public TransactionEvent(String sender, String receiver, Double amount) {
+        this.sender = sender;
+        this.receiver = receiver;
+        this.amount = amount;
+    }
+
+    public String getSender() { return sender; }
+    public String getReceiver() { return receiver; }
+    public Double getAmount() { return amount; }
+}
