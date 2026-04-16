88eb4f78a53b6ea84e45be4d21a3d37df9b60ce7
UJJU0405 <pandeyujju536@gmail.com>
2026-04-16 18:05:17 +0530

    Create KafkaProducer.java

---
+++ event/KafkaProducer.java
@@ -0,0 +1,18 @@
+package com.fintech.ewallet.event;
+
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaProducer {
+
+    private final KafkaTemplate<String, Object> kafkaTemplate;
+
+    public KafkaProducer(KafkaTemplate<String, Object> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void publishTransaction(TransactionEvent event) {
+        kafkaTemplate.send("transaction-topic", event);
+    }
+}
